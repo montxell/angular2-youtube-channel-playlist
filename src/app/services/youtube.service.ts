19cc3060ee13f9067df65aef8f29568368024f27
Montse Ariso <montxelltech@gmail.com>
2017-06-24 01:36:00 +0200

    Add 'getVideos' method and subscribe to use it

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@angular/core';
 
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/Rx';
+
+
 @Injectable()
 export class YoutubeService {
 
-  constructor() { }
+  private youtubeUrl: string = "https://www.googleapis.com/youtube/v3"
+  private apiKey: string = "AIzaSyCZ-5zd5gcLxX4Ja5L5fFCvHW3vlS5Blno";
+  private playlist: string = "UU9zX2xZIJ4cnwRsgBpHGvMg";
+
+  private nextPageToken: string = "";
+
+  constructor( public http: Http ) { }
+
+  getVideos() {
+
+    let url = `${ this.youtubeUrl }/playlistItems`;
+
+    let params = new URLSearchParams();
+
+    params.set( 'part', 'snippet');
+    params.set( 'maxResults', '10');
+    params.set( 'playlistId', this.playlist);
+    params.set( 'key', this.apiKey);
+
+    return this.http.get( url, { search: params } )
+            .map( res => {
+              console.log( res.json() );
+              this.nextPageToken = res.json().nextPageToken;
+
+              let videos: any[] = [];
+              for ( let video of res.json().items ) {
+                let snippet = video.snippet;
+                videos.push(snippet);
+              }
+
+              return videos;
+              
+            });
+  }
+
+
 
 }
