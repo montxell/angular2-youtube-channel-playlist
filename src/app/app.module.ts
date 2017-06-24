c2da9f8482cba9f905dcadbe295cacd6831a82a6
Montse Ariso <montxelltech@gmail.com>
2017-06-24 19:44:29 +0200

    Add 'youtubeVideo' pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { YoutubeService } from './services/youtube.service';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { YoutubeVideoPipe } from './pipes/youtube-video.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    YoutubeVideoPipe
   ],
   imports: [
     BrowserModule,
